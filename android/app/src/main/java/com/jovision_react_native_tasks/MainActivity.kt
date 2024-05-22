6edadf66087c008265a5032d1948cad8d3c286ea
Abdul-Rahman Ammourah <abdulrhmanammourah@gmail.com>
2024-05-22 22:41:46 +0300

    Initial commit
    Generated by react-native@0.74.1

---
+++ android/app/src/main/java/com/jovision_react_native_tasks/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.jovision_react_native_tasks
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "JoVision_react_native_tasks"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
