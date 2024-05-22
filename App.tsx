cb3d558e95a18577d62f20cb34f9972e7dc57c99
Abdul-Rahman Ammourah <abdulrhmanammourah@gmail.com>
2024-05-22 22:51:11 +0300

    Changing

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React,{Component,useState} from "react";
 import {View,Text,Button,StyleSheet,Alert} from 'react-native';
 import Task16 from "./Tasks/Task16";
+import Task17 from "./Tasks/Task17";
 
 export default class App extends Component {
   render(){
     return(
 
       <View>
-        <Task16></Task16>
+        {/* <Task16></Task16> */}
+        <Task17></Task17>
       </View>
 
     )
---
+++ Tasks/Task17.js
@@ -0,0 +1,33 @@
+import React,{Component,useState} from "react";
+import {View,Text,Button,StyleSheet,Alert} from 'react-native';
+ 
+const Task17 = () => {
+    const [IsHidden,SetIsHidden] = useState(true);
+
+    const HandelPress = () => {
+        SetIsHidden(!IsHidden);
+    }
+    
+    return(
+
+        <View style={style.container}>
+            {IsHidden && <Text style={style.text}>Abdul-Rahman Ammourah</Text>}
+            <Button title="Show" onPress={HandelPress} ></Button>
+            
+        </View>
+        
+    )
+}
+const style = StyleSheet.create({
+    container: {
+        flex : 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text:{
+        margin: 20
+
+    }
+});
+
+export default Task16;
